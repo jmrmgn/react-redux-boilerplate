9bf75785c4a7931a680621db8b8714ef8a8995a3
Jomar <jomar.magno026@gmail.com>
2018-12-30 01:24:16 +0800

    boilerplate added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+   render() {
+      return (
+         <div className="App">
+            <div>Age: {this.props.age}</div>
+            <button onClick={this.props.onAgeUp}>Age Up</button>
+            <button onClick={this.props.onAgeDown}>Age Down</button>
+         </div>
+      );
+   }
 }
 
-export default App;
+const mapStateToProps = state => {
+   return {
+      age: state.age
+   }
+}
+
+const mapDispatchToProps = (dispatch) => {
+   return {
+      onAgeUp: () => dispatch({ type: 'AGE_UP', value: 1 }),
+      onAgeDown: () => dispatch({ type: 'AGE_DOWN', value: 1 })
+   }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);

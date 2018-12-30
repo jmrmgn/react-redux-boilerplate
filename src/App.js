3c7892ba53786ab4d1e87bb156df539f9ebc2cc1
Jomar <jomar.magno026@gmail.com>
2018-12-30 14:19:41 +0800

    DEL_ITEM and CLEAR_HISTORY added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,22 @@ class App extends Component {
             <div>Age: {this.props.age}</div>
             <button onClick={this.props.onAgeUp}>Age Up</button>
             <button onClick={this.props.onAgeDown}>Age Down</button>
+            <hr/>
+            <h4>History</h4>
+            <button onClick={this.props.onClearHistory}>Clear</button>
+            <div>
+               <ul>
+                  {
+                     this.props.history.map((el) => {
+                        return (
+                           <li key={el.id} className="historyItem" onClick={() => this.props.onDeleteItem(el.id)} >
+                              {el.age}
+                           </li>
+                        );
+                     })
+                  }
+               </ul>
+            </div>
          </div>
       );
    }
@@ -17,14 +33,17 @@ class App extends Component {
 
 const mapStateToProps = state => {
    return {
-      age: state.age
+      age: state.age,
+      history: state.history
    }
 }
 
 const mapDispatchToProps = (dispatch) => {
    return {
       onAgeUp: () => dispatch({ type: 'AGE_UP', value: 1 }),
-      onAgeDown: () => dispatch({ type: 'AGE_DOWN', value: 1 })
+      onAgeDown: () => dispatch({ type: 'AGE_DOWN', value: 1 }),
+      onDeleteItem: (id) => dispatch({ type: 'DEL_ITEM', key: id }),
+      onClearHistory: () => dispatch({ type: 'CLEAR_HISTORY' })
    }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,16 +1,39 @@
 const initialState = {
-   age: 21
+   age: 21,
+   history: []
 };
 
 const reducer = (state = initialState, action) => {
    const newState = {...state};
 
    if (action.type === 'AGE_UP') {
-      newState.age += action.value;
+      return {
+         ...state,
+         age: state.age + action.value,
+         history: state.history.concat({id: Math.random(), age: state.age + action.value})
+      }
    }
 
    if (action.type === 'AGE_DOWN') {
-      newState.age -= action.value;
+      return {
+         ...state,
+         age: state.age - action.value,
+         history: state.history.concat({id: Math.random(), age: state.age - action.value})
+      }
+   }
+
+   if (action.type === 'DEL_ITEM') {
+      return {
+         ...state,
+         history: state.history.filter(el => el.id !== action.key)
+      }
+   }
+
+   if (action.type === 'CLEAR_HISTORY') {
+      return {
+         ...state,
+         history: []
+      }
    }
 
    return newState;

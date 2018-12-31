1991f60d2f9901f22f393af1f1fc7e688732a506
Jomar <jomar.magno026@gmail.com>
2018-12-31 16:39:51 +0800

    ageup saga added

--- src/index.js
+++ src/index.js
@@ -6,9 +6,22 @@ import * as serviceWorker from './serviceWorker';
 import reducer from './store/reducer';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+import { watchAgeUp } from './sagas/saga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+   reducer,
+   compose(
+      applyMiddleware(sagaMiddleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+   )
+);
+
+sagaMiddleware.run(watchAgeUp);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 serviceWorker.unregister();
